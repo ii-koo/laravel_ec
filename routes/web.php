8b1f6166aa652fbbc55a7c0e10cbdfd57855eff0
ii-koo <mhdihsan24@gmail.com>
2022-12-07 06:06:54 +0700

    installed jetstream and jetstream:livewire

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,12 @@ use App\Http\Controllers\Home;
 //     return view('home');
 // });
 Route::get('/', [Home::class, 'index']);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
